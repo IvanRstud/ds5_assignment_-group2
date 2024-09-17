643295e8cb08b781be35fc3556635088a9f3834f
ismailsakhi <500218@student.fontys.nl>
2024-09-17 11:33:47 +0200

    Changes ex1 2

--- MainHW.py
+++ MainHW.py
@@ -1,7 +1,17 @@
 #homework week 2 / group 2 
 #ex1
-def read_file(file_path):
-    #reads the file mentioned in the path, and returns the list with records
+def read_file(file_path: str) -> list:
+    """
+    reads the file mentioned in the path, and returns the list with records
+
+    Input:
+    file_path (str)
+        the path to a file you want to read
+
+    Output:
+    records (list)
+        list of all records
+    """
     records = []
     with open(file_path, 'r') as file:
         csv_reader = csv.DictReader(file)
@@ -20,6 +30,8 @@ def filter_results(records):
     #filter the grades by the 80.0 grade 
     filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
     return(filtered_records)
+
+
 def print_results(records):
     #prints the student report for the filtered records.
     print("Student Report")
@@ -28,18 +40,25 @@ def print_results(records):
         print(f"Name: {record['Name']}")
     print(f"Grade: {record['Grade']}")
     print("--------------------")
+
+
 def main():
     #Main function to run the code generally 
     file_path = input("Enter the path to the CSV file: ")
+
     #request the input of the path to the CSV file with students' data 
     records = read_file(file_path)
+
     #runs the function and creates the list with records
     total,average = total_and_avarege_calculation (records)
+
     #run the function of total and avarege  
     print(f"Average Grade: {average}")
     print("--------------------")  
+
     #prints avarege grade 
     filtered_records = filter_results(records)
+
     #creates a list with records where the grade is greater than or equal to 80
     print_results(filtered_records)
     #print record of filtered results 
